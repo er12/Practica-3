446c1830214c91d19ca1f6f73fc6174d083c2fdd
Francis <francisangeles3@hotmail.com>
2016-06-10 18:32:33 -0400

    Eliminacion de articulo en cascada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main {
     {
         staticFileLocation("recursos");
         Manejador bd = new Manejador();
-        //bd.eliminarTodo();
+       // bd.eliminarTodo();
         bd.subir();
         Configuration configuration = new Configuration();
         configuration.setClassForTemplateLoading(Main.class, "/templates");
@@ -30,7 +30,7 @@ public class Main {
 
 
         //Administradores
-       // bd.insertarUsuario(new Usuario("er12","Ernesto Rodriguez","1234", false,true));
+        //bd.insertarUsuario(new Usuario("er12","Ernesto Rodriguez","1234", false,true));
         //bd.insertarUsuario(new Usuario("francis","Francis Cáceres","1234",true,true));
 
         //Usuario francis = new Usuario("","","",false,false);
@@ -143,6 +143,8 @@ public class Main {
 
         get("/articulos", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
+            Session sesion = request.session(true);
+            attributes.put("sesion",sesion.attribute("sesion").toString());
             int id = Integer.valueOf(request.queryParams("id"));
 
 
